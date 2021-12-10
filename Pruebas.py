ac67001cf2118e5d4581a5d1f38d1db62700ffcc
Fangio123 <carlosflamini@gmail.com>
2021-12-10 23:20:55 +0000

    version de tablas

--- Pruebas.py
+++ Pruebas.py
@@ -2,6 +2,7 @@ import json
 import requests
 requests.packages.urllib3.disable_warnings()
 import urllib.parse
+from prettytable import PrettyTable
 
 ###para obtener las organizaciones ME RESULTA SIN PROBLEMAS### 
 
@@ -12,38 +13,89 @@ resp = requests.get(api_url,headers=headers,verify=False)
 print("Request status:",resp.status_code)
 
 response_json = resp.json()
-print(json.dumps(response_json, indent=4))
+###imprimo el Json como  tabla
+
+
+table = PrettyTable(["Id","Organizaciones"])
+for Organiz in response_json:
+    table.add_row([Organiz["id"],Organiz["name"]])
+
+print(table)
+
+###buesco el id de organizacion  ###
+
+IdOrg = response_json[3]["id"]
+
+print (IdOrg)
 
 
 
-###en este paso donde surje el error 404 ###
-
-main_api="https://api.meraki.com/api/v1/organizations"
+main_api="https://api.meraki.com/api/v1/organizations/"
 headers={"X-Cisco-Meraki-API-Key":"6bec40cf957de430a6f1f2baa056b99a4fac9ea0"}
 
-###le asigno a organizacion un numero fijo 463308 *solo para 
-###probar  , 
-###que en realidad lo obtengo de recorrer el 1er requests 
-###json , y se lo asignaria a la variable Organizacion
-### mediante la funcion recursiva que vimos en clase .
+
+print ("Obteniendo las redes de la organizacion id" ,IdOrg)
+api_url2=main_api+(IdOrg)+'/networks'
+
+###VERIFICO LA URL
+###print(f"URL:{api_url2}")
 
 
-Organizacion=463308
-Redes="/networks"
-
-###Construyo la nueva URL con la organizacion elejida 
-###para obteener todas las redes de esa organizacion 
-
-### ES PROBABLE QUE AQUI SE ENCUENTRE EL ERROR 
-
-
-api_url2=main_api + urllib.parse.urlencode({"/":Organizacion})+Redes
 
 resp2 = requests.get(api_url2,headers=headers,verify=False)
 print("Request status:",resp2.status_code)
 
 response_json2 = resp2.json()
-print(json.dumps(response_json2, indent=4))
+
+tableRedes = PrettyTable(["id","Redes"])
+for redes in response_json2:
+    
+    tableRedes.add_row([redes["id"],redes["name"]])
+
+print(tableRedes)
+
+
+
+
+
+
+###Obtengo el id de la red y consulto sus  dispositivos .
+IdNet = response_json2[0]["id"]
+print (IdNet)
+main_api2="https://api.meraki.com/api/v1/networks/"
+
+###Armado de la URL###
+api_url3=main_api2+(IdNet)+'/devices'
+
+
+resp3 = requests.get(api_url3,headers=headers,verify=False)
+print("Request status:",resp3.status_code)
+response_json3 = resp3.json()
+###print(json.dumps(response_json3, indent=4))
+
+### armo la tabla ###
+
+tableDisp = PrettyTable(["Direccion","Nroserie"])
+for Dispositivos in response_json3:
+    
+    tableDisp.add_row([Dispositivos["address"],Dispositivos["serial"]])
+
+print(tableDisp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
